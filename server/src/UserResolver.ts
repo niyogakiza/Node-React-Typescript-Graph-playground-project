af25528269fa6e72959c2430e0935220b71842fa
Aniyogakiza <aimable.niyogakiza@drpgroup.com>
2019-10-01 19:06:03 +0100

    Setup login - Cookies - Tokens

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,14 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql'
-import { hash } from 'bcryptjs'
+import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx } from 'type-graphql'
+import { hash, compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 import { User } from './entity/User'
+import { MyContext } from './MyContext'
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -30,5 +38,42 @@ export class UserResolver {
       return false
     }
     return true
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() { res }: MyContext
+  ) : Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email }});
+    if(!user) throw new Error('Could not find the user')
+    const valid = await compare(password, user.password)
+    if(!valid) throw new Error('Password did not match')
+    // login successful
+    /**
+     * Refresh token
+     */
+    res.cookie(
+      "aiam",
+      sign({
+        userId: user.id, 
+      }, 'asdasdasdsdsd', {
+        expiresIn: '30d'
+      }),
+      {
+        httpOnly: true,
+      }
+    )
+    /**
+     * Create token
+     */
+    return {
+      accessToken: sign({
+        userId: user.id, 
+      }, 'asdasdasd', {
+        expiresIn: '15m'
+      })
+    }
   }
 }
